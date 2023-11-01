2d7e6fc44a5de769d64df2439b29f312ca2164d7
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-11-01 09:43:15 -0600

    pseudocode

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -21,4 +21,16 @@ if (require.main === module) {
 module.exports = findShortestStringRecursive;
 
 // Please add your pseudocode to this file
+/**
+ * given array of length n, split the array into two arrays of length n/2
+ * if the new array is length 1 or 2, compare string lengths and return the shorter 
+ * or lower index if length is the same
+ * else split the arrays again
+ */
 // And a written explanation of your solution
+/**
+ * this problem seems like a good opportunity to implement a divide and conquer algorithm
+ * splitting the array in half if the array has more than two elements, and if it doesnt then
+ * return the shortest (or if odd # of items return the only) string in the array
+ * 
+ */
